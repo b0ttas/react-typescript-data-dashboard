511efaac3efe1133ee6cbb747abde6b4f21dbb32
bottas <dev.bottas@gmail.com>
2019-06-21 07:05:21 +0100

    Code polishing, UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,71 @@
 import React from 'react';
 import logo from './logo.svg';
+import brand from './brand.svg';
+import spacer from './spacer.svg';
+import plus from './plus.svg';
+import area from './area.svg';
+import sensor from './sensor.svg';
+
 import './appstyle.scss';
 
-const App: React.FC = () => {
-  
-  function openNav() {
-    //document.getElementById("mySidenav").style.width = "250px";
-    //document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  
-  function closeNav() {
-    //document.getElementById("mySidenav").style.width = "0";
-    //document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-  }
+function TopNav() {
+  let addPath, addHref = null;
+  let navTitle = null;
 
+  addHref = window.location.pathname.substring(1);
+
+  switch (addHref) {
+    case "areas":
+      navTitle = "Áreas";
+      addHref = window.location.pathname.substring(1) + "-add";
+      addPath = <a id="add" href={addHref}>Adicionar<img id="add-img" src={plus} className="App-plus" alt="plus"/></a>    
+      break;
+    case "sensores":
+      navTitle = "Sensores";
+      addHref = window.location.pathname.substring(1) + "-add";
+      addPath = <a id="add" href={addHref}>Adicionar<img id="add-img" src={plus} className="App-plus" alt="plus"/></a>    
+      break;
+    case "areas-add":
+      navTitle = "Áreas";
+      addHref = "";
+      break;
+    case "sensores-add":
+      navTitle = "Sensores"
+      addHref = "";
+      break;
+    }
+
+return (
+  <div className="topnav">
+  <span id="menu-span">&#9776;</span>
+  <a href="/" id="brand"><img src={brand} className="App-brand" alt="brand"/></a>
+  <span><img src={spacer} alt=""/></span>
+  <a className="active" href={window.location.href}>{navTitle}</a>
+  {addPath} 
+</div>
+)
+}
+
+function SideNav() {
   return (
+    <div className="sidenav">
+    <a href="areas"><img src={area} alt="area"/>Áreas</a>
+    <a href="sensores"><img src={sensor} alt="sensor"/>Sensores</a>
+  </div>
+  )
+}
 
+const App: React.FC = () => {
+  return (
     <div className="App">
-
-      <div className="topnav">
-        <a id="clickable" onClick={openNav}> <span>&#9776;</span> </a>
-        <a> logo here </a>
-        <a className="active" href="#areas">Áreas</a>
-      </div>
-
-      <div id="mySidenav" className="sidenav">
-        <a href="#" className="closebtn" onClick={closeNav}>&times;</a>
-        <a href="#areas">Áreas</a>
-        <a href="#sensores">Sensores</a>
-      </div>
-
-      <header className="App-header">
+    <TopNav/>
+    <SideNav/>
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           In construction.
         </p>
-
-      </header>
+      </div>
     </div>
   );
 }
