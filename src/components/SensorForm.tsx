38a389fec00f4a738d6c34dc9af2c8f25f28b430
bottas <dev.bottas@gmail.com>
2019-07-02 01:33:33 +0100

    Minor form changes

--- src/components/SensorForm.tsx
+++ src/components/SensorForm.tsx
@@ -1,39 +1,36 @@
 import * as React from 'react';
 import "./SensorForm.scss";
 
-
+//<label> for edit forms
 
 function SensorForm() {
   return (
     <div className="sensorform">
         <form>
             <fieldset>
-            <input id="areaName" type="text" name="areaName" required/>
-            <label>Nome da área *</label>
+            <input id="areaName" type="text" name="areaName" placeholder="Nome da área *" required/>
             <div className="after"></div>
             </fieldset>
             <fieldset>
-            <input id="areaCulture" type="text" name="areaCulture" />
-            <label>Nome da área *</label>
+            <input id="areaCulture" type="text" name="areaCulture" placeholder="Nome da área *" required/>
             <div className="after"></div>
             </fieldset>
             <fieldset>
-            <input id="areaAcres" type="text" name="areaAcres" />
-            <label>Nome da área *</label>
+            <input id="areaAcres" type="text" name="areaAcres" placeholder="Nome da área *" required/>
             <div className="after"></div>
             </fieldset>
             <fieldset>
-            <select id="deviceUID">
-                <option value="device1">device1</option>
-                <option value="device2">device2</option>
-                <option value="device3">device3</option>
-                <option value="device4">device4</option>
+            <select className="select_box" id="deviceUID" required>
+                <option value="" disabled selected hidden>Sensor *</option>
+                <option value="0">device2</option>
+                <option value="1">device3</option>
+                <option value="2">device4</option>
             </select>
-            <label>Nome da área *</label>
             <div className="after"></div>
             </fieldset>
             <fieldset>
-        <button id="submit" />
+        <button>Cancelar</button>
+        <button id="submit">Guardar</button>
         </fieldset>
         </form>
     </div>
